5af2841c3403babe16730cad3ab23edf74b2b4d9
jonana-split <127276862+jonana-split@users.noreply.github.com>
2023-03-21 20:36:59 -0400

    Add files via upload

---
+++ lab06.py
@@ -0,0 +1,54 @@
+# Joanna Mijares
+# Lab 06 Encoding/ Decoding
+
+def menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+
+def encode_pass(password):
+    encoded_pass = ''
+    for i in password:
+        if i.isdigit():
+            encoded_pass += str(int(i) + 3)
+        else:
+            pass
+    return encoded_pass
+
+
+def decode_pass(encoded_pass):
+    decoded_pass = ''
+    for i in encoded_pass:
+        if i.isdigit():
+            decoded_pass += str(int(i) - 3)
+        else:
+            pass
+    return decoded_pass
+
+
+if __name__ == "__main__":
+
+    while True:
+
+        menu()
+        print("")
+
+        menu_option = int(input("Please enter an option: "))
+
+        if menu_option == 1:
+            password = input("Please enter your password to encode: ")
+            encoded_pass = encode_pass(password)
+            print("Your password has been encoded and stored!")
+            print("")
+
+        elif menu_option == 2:
+            print(f"The encoded password is {encoded_pass}, and the original password is", end=' ')
+            decoded_pass = decode_pass(encoded_pass)
+            print(decoded_pass)
+            print("")
+
+        elif menu_option == 3:
+            break
