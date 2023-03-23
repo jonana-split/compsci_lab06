76d97fc9d9790e0676785fa128ce70a0facd43a3
jonana-split <127276862+jonana-split@users.noreply.github.com>
2023-03-22 23:15:29 -0400

    Update lab06.py

--- lab06.py
+++ lab06.py
@@ -1,14 +1,17 @@
 # Joanna Mijares
 # Lab 06 Encoding/ Decoding
 
-def menu():
+# Lab partner is Sallie Zhou
+
+def menu(): # Prints out menu
     print("Menu")
     print("-------------")
     print("1. Encode")
     print("2. Decode")
     print("3. Quit")
 
-
+# Encodes the password by checking if each character in the password string is a number. If it is indeed a number, then it will be converted to an integer,
+# 3 will be added, and then placed back into a string.
 def encode_pass(password):
     encoded_pass = ''
     for i in password:
@@ -18,7 +21,8 @@ def encode_pass(password):
             pass
     return encoded_pass
 
-
+# Decodes the password by checking if each character in the password string is a number. If it is indeed a number, then it will be converted to an integer,
+# 3 will be subtracted, and then placed back into a string.
 def decode_pass(encoded_pass):
     decoded_pass = ""
     for digit in encoded_pass:
@@ -29,6 +33,7 @@ def decode_pass(encoded_pass):
 
 if __name__ == "__main__":
 
+    # While True will ensure that the main function loops until it breaks
     while True:
 
         menu()
@@ -36,14 +41,19 @@ if __name__ == "__main__":
 
         menu_option = int(input("Please enter an option: "))
 
+        # If 1 is inputted, then you will be asked to input a passcode which will then be encoded by my encode_pass function
         if menu_option == 1:
             password = input("Please enter your password to encode: ")
             encoded_pass = encode_pass(password)
             print("Your password has been encoded and stored!")
             print("")
+        
+        # If 2 is inputted, the previously encoded function will be decoded and both the encoded and decoded passwords will be read back to you.
         if menu_option == 2:
             decoded_pass = decode_pass(encoded_pass)
             print(f"The encoded password is {encoded_pass}, and the original password is {decoded_pass}.")
             print()
+            
+        # Quits the program
         elif menu_option == 3:
             break
